0061f1771f4cd42bcf9c3cfe648deeda8da6a94b
RaelCappra <rael.cappra@gmail.com>
2015-10-14 04:20:01 -0300

    implementado DisciplinaDao.getByTutor

--- database/DisciplinaDao.php
+++ database/DisciplinaDao.php
@@ -15,7 +15,7 @@ class DisciplinaDao {
 
         $dbCon = $conexao->getConexao();
         //criar join com curso
-        $sql = "select $tabela.id_disciplina $tabela.nome, $tabela.curso from $tabela " .
+        $sql = "select $tabela.id_disciplina, $tabela.nome, $tabela.curso from $tabela " .
                 "join $disciplina on id_curso = curso";
 
         $result = pg_query($dbCon, $sql);
@@ -50,11 +50,11 @@ class DisciplinaDao {
         $sql = "select * from " . self::$tabela . " where id_disciplina = $1";
 
         $result = pg_query_params($dbCon, $sql, Array($id));
-        
+
         $disciplina = 0;
-        
+
         $linha = pg_fetch_assoc($result);
-        if($linha){
+        if ($linha) {
             $nome = $linha['nome'];
             $idCurso = $linha['curso'];
 
@@ -64,7 +64,7 @@ class DisciplinaDao {
             $disciplina->setNome($nome);
             $disciplina->setCurso($curso);
         }
-        
+
         $conexao->closeConexao();
 
         return $disciplina;
@@ -74,7 +74,7 @@ class DisciplinaDao {
         $conexao = new Conexao();
 
         $dbCon = $conexao->getConexao();
-        
+
         $sql = "insert into disciplina (nome, curso) values($1, $2)";
         $params = array($disciplina->getNome(), $disciplina->getCurso()->getId());
         pg_query_params($dbCon, $sql, $params);
@@ -105,6 +105,28 @@ class DisciplinaDao {
         pg_query_params($dbCon, $sql, $params);
 
         $conexao->closeConexao();
+    }
+
+    function listByTutor($tutor) {
+        $conexao = new Conexao();
+        
+        $tutor_disciplina = "sistutor.tutor_disciplina";
+        $dbCon = $conexao->getConexao();
+        //criar join com curso
+        $sql = "select disciplina, from $tutor_disciplina where tutor=$1";
+
+        $result = pg_query_params($dbCon, $sql, Array($tutor->getTutorId()));
+
+        $disciplinas = Array();
+        while ($linha = pg_fetch_assoc($result)) {
+            $id = $linha['disciplina'];
+            $disciplina = $this->getById($id);
+            array_push($disciplinas, $disciplina);
+        }
+
+        $conexao->closeConexao();
+
+        return $disciplinas;
     }
 
 }
