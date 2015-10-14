763210bcd7918d5da7682fde19bed3d04b058e41
RaelCappra <rael.cappra@gmail.com>
2015-10-14 11:02:30 -0300

    bootstrap um pouco melhor no login

--- view/index.php
+++ view/index.php
@@ -17,11 +17,12 @@ if (checkLogin()){
     <body>
         <div class="container">
             <form action="../controller/FrontController.php" method="post">
-                Login:<input type="text"  name="login"><br>
-                Senha:<input type="password" name="senha"><br>
+                Login:<input class="form-control" type="text"  name="login"><br>
+                Senha:<input class="form-control" type="password" name="senha"><br>
                 <input type="hidden" name="controller" value="GestorController">
                 <input type="hidden" name="action" value="login()">
                 <input type="submit" value="Logar">
+                </div
             </form>
         </div>
     </body>
