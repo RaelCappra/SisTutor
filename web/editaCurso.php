1a5553aa101ef34e77b6928c4398055aee9070d0
Razue <lucas.viana.rs@hotmail.com>
2015-10-13 23:08:24 -0300

    Começo dos editar Template

--- web/editaCurso.php
+++ web/editaCurso.php
@@ -1,8 +1,43 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    require_once '../controller/CursoController.php';
+    require_once '../controller/PoloController.php';
+    require_once("../lib/raelgc/view/Template.php");
+    use raelgc\view\Template;
 
+    $tpl = new Template("../view/editaCurso.html");
+    
+    $cursoController = new CursoController();
+    $poloController = new PoloController();
+    $curso = $cursoController->getById($_GET['id']);
+    $tpl->NOME_CURSO = $curso->getNome();
+    
+    $tipos = $cursoController->getTipos();
+    $polos = $poloController->read();
+    
+    foreach($tipos as $t){
+        $tpl->ID_TIPO = $t["id_tipo_curso"];
+        $tpl->NOME_TIPO = $t["descricao"];
+        if($t["id_tipo_curso"] == $curso->getTipo()){ 
+           $tpl->SELECTED = "selected";       
+        } else {
+            $tpl->clear("SELECTED");
+        }
+        $tpl->block("BLOCK_TIPO");
+    }
+    
+    foreach($polos as $p){
+        $tpl->ID_POLO = $p->getId();
+        $tpl->NOME_POLO = $p->getNome();
+        
+        if($p->getId() == $curso->getPolo()){ 
+           $tpl->SELECTED = "selected";       
+        } else {
+            $tpl->clear("SELECTED");
+        }
+        
+        $tpl->block("BLOCK_POLO");
+        
+    }
+
+    $tpl->show();
